21edcdb35b104fd994b9788497c4ee7e79622269
Ashar <103361807+AsharAli999@users.noreply.github.com>
2023-10-04 23:53:06 +0500

    Create ERC_Token.sol

---
+++ ERC_Token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TheodoresToken is ERC20("Theodores Token", "TT"), Ownable {
+    constructor() {}
+
+    function mintFifty() public onlyOwner {
+        _mint(msg.sender, 50 * 10**18);
+    }
+}
