dc51cb8b51c8323d40ebbe37d85687b0f697e5a5
Ashar <103361807+AsharAli999@users.noreply.github.com>
2023-10-04 23:55:01 +0500

    Update ERC_Token.sol

--- ERC_Token.sol
+++ ERC_Token.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TheodoresToken is ERC20("Theodores Token", "TT"), Ownable {
-   
+contract TheodoresToken is ERC20("Theodores Token", "TT"), Ownable {  
 constructor() {}
 
     function mintFifty() public onlyOwner {
